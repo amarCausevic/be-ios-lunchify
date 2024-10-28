f5464be5e2c967fd0f8d022d3ed977f5dc110e93
Amar Causevic <amar.causevic01@gmail.com>
2024-10-28 21:27:35 +0100

    LUNCHIFY-BE-2: Add project files and setup
    Added initial files to the project

---
+++ src/main/java/com/lunchify/server/LunchifyApplication.java
@@ -0,0 +1,13 @@
+package com.lunchify.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchifyApplication.class, args);
+	}
+
+}
