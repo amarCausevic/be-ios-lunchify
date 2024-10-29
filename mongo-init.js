47b22251dfde2a974049d7125af363de384e6bb3
Amar Causevic <amar.causevic01@gmail.com>
2024-10-29 20:56:40 +0100

    LUNCHIFY-BE-4: Full database initialisation
    Added env-example has been added to the repo
    Added .env to the gitignore
    Added full mongo-init.js functionality
    Updated docker-compose file

--- mongo-init.js
+++ mongo-init.js
@@ -1,12 +1,64 @@
-db.createUser(
-        {
-            user: "admin",
-            pwd: "admin",
-            roles: [
-                {
-                    role: "readWrite",
-                    db: "lunchify"
-                }
-            ]
-        }
-);
+try {
+  console.log("INITIALIZE DATABASE");
+  db.createUser(
+          {
+              user: "cmar",
+              pwd: "cmarIsYou",
+              roles: [
+                  {
+                      role: "readWrite",
+                      db: "lunch"
+                  }
+              ]
+          }
+  );
+
+  db.createCollection("meals");
+  db.createCollection("users");
+  db.createCollection("orders");
+
+  console.log("INSERTING MEALS DATA INTO DATABASE");
+  db.meals.insertOne(
+    {
+      name: "Burek",
+      displayName: "Burek - The One",
+      ingredients: "Puff-Pastery, Minced Beef, Onions, Oil",
+      cautionIngredients: "Not for vegans",
+      comments: "Not suitable for Vegans and Vegetarians",
+      timestampCreated: new Date(),
+      timestampPrepareOn: new Date()
+    }
+  );
+
+  console.log("INSERTING USERS DATA INTO DATABASE");
+  db.users.insertOne(
+    {
+      name: "Cmarodej Cmar",
+      displayName: "Cmarodej",
+      email: "cmarizard@gmail.com",
+      device: "IOS 18.1",
+      active: true
+    }
+  );
+
+  console.log("INSERTING ORDERS DATA INTO DATABASE");
+  db.orders.insertOne(
+    {
+      userName: "Cmarodej Cmar",
+      userId: ObjectId("67213c3cbd0371c86659139f"),
+      meals: [
+         {
+            id: ObjectId("67213b774d39d98aaf59139e"),
+            name: "Burek",
+            timestampCreated: new Date(),
+            timestampPrepareOn: new Date()
+          }],
+      status: "SUBMITTED",
+      timestampCreated: new Date(),
+      userComment: "Can I get the re-heated one"
+    }
+  );
+
+} catch (error) {
+  console.error(error);
+}
