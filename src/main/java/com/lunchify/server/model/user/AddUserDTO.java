0e85abc68af6bc0eb1a03c47d3532f7e3091c8e6
Amar Causevic <amar.causevic01@gmail.com>
2024-11-01 08:55:53 +0100

    LUNCHIFY-BE-11:
    Moved all filed in controller, service, model package into respected packages
    Added functionalities for adding, updating, fetching single, deleting single user
    Small refactoring

--- src/main/java/com/lunchify/server/model/user/AddUserDTO.java
+++ src/main/java/com/lunchify/server/model/user/AddUserDTO.java
@@ -1,19 +1,16 @@
-package com.lunchify.server.model;
+package com.lunchify.server.model.user;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Document(collection = "users")
-public class UserDTO {
+public class AddUserDTO extends UserDTO {
 
-  @Id
-  private String id;
   private String name;
   private String displayName;
   private String email;
