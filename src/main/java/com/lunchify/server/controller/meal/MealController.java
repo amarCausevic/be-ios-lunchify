1c63caeadf9c7c0ca541af86a7992b289999656f
Amar Causevic <amar.causevic01@gmail.com>
2024-11-04 08:10:31 +0100

    LUNCHIFY-BE-14: Initial files for Meal actions
    DeleteUserDTO was renamed into DeleteReponseDTO and moved to common folder
    Meal service, controller, DTO and repository were populated with all actions needing for basic CRUD operations

--- src/main/java/com/lunchify/server/controller/meal/MealController.java
+++ src/main/java/com/lunchify/server/controller/meal/MealController.java
@@ -1,5 +1,19 @@
 package com.lunchify.server.controller.meal;
 
+import com.lunchify.server.model.common.DeleteResponseDTO;
+import com.lunchify.server.model.meal.AddMealDTO;
+import com.lunchify.server.model.meal.MealDTO;
+import com.lunchify.server.service.meal.MealServiceI;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +21,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/meal")
 public class MealController {
 
+  @Autowired
+  MealServiceI mealServiceI;
+
+  @PostMapping
+  public ResponseEntity<AddMealDTO> addMeal(@RequestBody AddMealDTO requestBody) {
+    return new ResponseEntity<AddMealDTO>(mealServiceI.addMeal(requestBody), HttpStatus.CREATED);
+  }
+
+  @GetMapping
+  public List<MealDTO> geMealList() {
+    return mealServiceI.getMeals();
+  }
+
+  @GetMapping("{id}")
+  public ResponseEntity<MealDTO> getMealDetails(@PathVariable("id") String id) {
+    return new ResponseEntity<MealDTO>(mealServiceI.getMealDetails(id), HttpStatus.OK);
+  }
+
+  @PatchMapping("{id}")
+  public ResponseEntity<MealDTO> updateMealDetails(@PathVariable("id") String id,
+      @RequestBody MealDTO requestBody) {
+    return new ResponseEntity<MealDTO>(mealServiceI.updateMealDetails(requestBody, id),
+        HttpStatus.OK);
+  }
+
+  @DeleteMapping("{id}")
+  public ResponseEntity<DeleteResponseDTO> deleteMeal(@PathVariable("id") String id) {
+    mealServiceI.deleteMeal(id);
+
+    return new ResponseEntity<DeleteResponseDTO>(
+        new DeleteResponseDTO("CRUD Delete Meal",
+            "Meal has been successfully delete from database"),
+        HttpStatus.OK);
+  }
 }
--- src/main/java/com/lunchify/server/service/meal/MealService.java
+++ src/main/java/com/lunchify/server/service/meal/MealService.java
@@ -1,5 +1,64 @@
 package com.lunchify.server.service.meal;
 
+import com.lunchify.server.model.meal.AddMealDTO;
+import com.lunchify.server.model.meal.MealDTO;
+import com.lunchify.server.repository.AddMealRepository;
+import com.lunchify.server.repository.MealRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MealService implements MealServiceI {
 
+  @Autowired
+  private MealRepository mealRepository;
+
+  @Autowired
+  private AddMealRepository addMealRepository;
+
+  @Override
+  public AddMealDTO addMeal(AddMealDTO addMealDTO) {
+    return addMealRepository.save(addMealDTO);
+  }
+
+  @Override
+  public List<MealDTO> getMeals() {
+    return mealRepository.findAll();
+  }
+
+  @Override
+  public MealDTO getMealDetails(String id) {
+    Optional<MealDTO> meal = mealRepository.findById(id);
+
+    if (meal.isPresent()) {
+      return meal.get();
+    }
+
+    throw new RuntimeException();
+  }
+
+  @Override
+  public MealDTO updateMealDetails(MealDTO updatedMeal, String id) {
+    MealDTO existingMeal = mealRepository.findById(id).orElseThrow(
+        RuntimeException::new
+    );
+
+    //TODO: Find more clean way to fill up all information needed to update meal information
+    existingMeal.setDisplayName(updatedMeal.getDisplayName());
+    existingMeal.setName(updatedMeal.getName());
+    existingMeal.setComments(updatedMeal.getComments());
+    existingMeal.setIngredients(updatedMeal.getIngredients());
+    existingMeal.setCautionIngredients(updatedMeal.getCautionIngredients());
+    mealRepository.save(existingMeal);
+
+    return existingMeal;
+  }
+
+  @Override
+  public void deleteMeal(String id) {
+    mealRepository.findById(id).orElseThrow(RuntimeException::new);
+    mealRepository.deleteById(id);
+  }
 }
